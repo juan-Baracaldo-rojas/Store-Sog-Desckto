f71c58ac378f006b5fa34330be655d9bd0147a4b
Juan Manuel Baracaldo Rojas <87029593+juan-Baracaldo-rojas@users.noreply.github.com>
2024-08-10 10:58:40 -0500

    create class SellDetail

--- model/Sell.py
+++ model/Sell.py
@@ -1,24 +1,23 @@
-class Sell():
-    def __init__(self,sell_date=None,sell_total=None):
-        self.sell_date=sell_date
-        self.sell_total=sell_total
+class Sell:
+    def __init__(self, sell_date=None, sell_total=None):
+        self._sell_date = sell_date
+        self._sell_total = sell_total
     
     @property
     def sell_date(self):
         return self._sell_date
     
     @sell_date.setter
-    def sell_date(self,value):
-        self._sell_date=value
+    def sell_date(self, value):
+        self._sell_date = value
     
     @property
     def sell_total(self):
         return self._sell_total
     
     @sell_total.setter
-    def sell_total(self,value):
-        self._sell_total=value
+    def sell_total(self, value):
+        self._sell_total = value
     
     def __str__(self):
-        return (f"\nsell_date: {self.sell_date} ,  sell_total: {self.sell_total}")
-    
+        return f"\nsell_date: {self.sell_date}, sell_total: {self.sell_total}"
