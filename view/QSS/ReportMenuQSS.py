822fbcab2e4e862718f5128efcd187ca09b22058
Juan Manuel Baracaldo Rojas <87029593+juan-Baracaldo-rojas@users.noreply.github.com>
2024-08-22 07:43:10 -0500

    creacion ventana menu reportes

---
+++ view/QSS/ReportMenuQSS.py
@@ -0,0 +1,28 @@
+def styleReportMenu():
+    return """
+          QPushButton {
+        background-color: #fff; 
+        border:1px solid #000;
+        border-radius:10%;
+        color: black;
+        padding: 10px 20px;
+        margin:2px;
+        font-family:sans-serif;
+        text-align: center;
+        text-decoration: none;
+        font-size: 16px;
+        margin: 4px 2px;
+        
+    }
+
+    QPushButton:hover {
+        background-color: #45a049; /* Verde oscuro */
+        color: white;
+    
+    }
+    
+    QPushButton:pressed {
+        color: white;
+        background-color: #3e8e41; /* Verde aún más oscuro */
+    }
+"""
